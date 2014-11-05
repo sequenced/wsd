78699f120db7f610bf6cbc2fc4054443419e5d46
mxg <mxg@sequencedsystems.com>
2014-11-05 22:42:46 +0800

    initial drop

---
+++ src/jen.c
@@ -0,0 +1,38 @@
+#include "jen.h"
+
+static int md=0;
+
+int
+jen_on_frame(wsconn_t *conn, wsframe_t *wsf, buf_t *in, buf_t *out)
+{
+  if (0>ssys_shmem_write(md, buf_ref(in), buf_len(in)))
+    perror("ssys_shmem_write");
+
+  return 0;
+}
+
+int
+jen_on_open(wsconn_t *conn)
+{
+  if (0>(md=ssys_shmem_open(conn->location->url,
+                            SSYS_SHMEM_FLAG_WRITE|SSYS_SHMEM_FLAG_CREATE,
+                            SSYS_SHMEM_MODE_PIPE)))
+    {
+      perror("ssys_shmem_open");
+      return -1;
+    }
+
+  return 0;
+}
+
+void
+jen_on_close(wsconn_t *conn)
+{
+  if (0>md)
+    return;
+
+  if (0>ssys_shmem_close(md))
+    perror("ssys_shmem_close");
+
+  md=0;
+}
---
+++ src/jen.h
@@ -0,0 +1,11 @@
+#ifndef __JEN_H__
+#define __JEN_H__
+
+#include <sshmem_api.h>
+#include "ws_interface.h"
+
+int jen_on_frame(wsconn_t *conn, wsframe_t *wsf, buf_t *in, buf_t *out);
+int jen_on_open(wsconn_t *conn);
+void jen_on_close(wsconn_t *conn);
+
+#endif /* #ifndef __JEN_H__ */
